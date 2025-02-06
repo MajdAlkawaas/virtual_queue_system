bf84c48a35e745f4555fa98e3fdb9893406e130f
Majd Alkawaas <mma250@mail.aub.edu>
2025-02-06 23:00:12 +0000

    registered the guests app in settings.py

--- virtual_queue_system/virtual_queue_system/settings.py
+++ virtual_queue_system/virtual_queue_system/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['virtual-queue-system.onrender.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'customers',
+    'guests',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
