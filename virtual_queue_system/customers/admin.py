1131e3f03570c42590baf7a76a1cf438a74a4a9e
Majd Alkawaas <mma250@mail.aub.edu>
2025-03-12 01:57:58 +0000

    Registered the customer app models in the admin system

--- virtual_queue_system/customers/admin.py
+++ virtual_queue_system/customers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from customers.models import Category, Operator, Queue, Manager, User, Customer
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+
+
+
+admin.site.register(User, UserAdmin)
+
+# Registering the models with the admin site
+admin.site.register(Category)
+admin.site.register(Queue)
+
+# Register Manager, Operator, and Customer if needed
+admin.site.register(Manager)
+admin.site.register(Operator)
+admin.site.register(Customer)
+
