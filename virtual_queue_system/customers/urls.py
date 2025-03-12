b7e9a74381e30be42c918efcaed4e40eea2566c3
Majd Alkawaas <mma250@mail.aub.edu>
2025-03-12 02:02:50 +0000

    Provided the irl_patterns for: - manager_singup - operator_singup - login - logout - manager_dashboard - operator_dashboard

--- virtual_queue_system/customers/urls.py
+++ virtual_queue_system/customers/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [ 
+    path('signup/manager/', views.manager_signup, name='manager_signup'),
+    path('signup/operator/', views.operator_signup, name='operator_signup'),
+    path('login', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 
+
+    path('dashboard', views.manager_dashboard, name='manager_dashboard'),
+    path('operator_dashboard', views.operator_dashboard, name='operator_dashboard'),
 ]
