c169ac0d018ddb50d85bc589b099f55f3e29999e
Majd Alkawaas <mma250@mail.aub.edu>
2025-03-13 21:28:08 +0000

    Added the decorators.py file in the customers app, it is for access control.

---
+++ virtual_queue_system/customers/decorators.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from functools import wraps
+
+def manager_required(view_func):
+    @wraps(view_func)
+    @login_required
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_manager:
+            return view_func(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('forbidden'))  # Or another restricted access page
+    return _wrapped_view
+
+def operator_required(view_func):
+    @wraps(view_func)
+    @login_required
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_operator:
+            return view_func(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('forbidden'))
+    return _wrapped_view
+
+
+def customer_required(view_func):
+    @wraps(view_func)
+    @login_required
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_customer:
+            return view_func(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('forbidden'))
+    return _wrapped_view
+
