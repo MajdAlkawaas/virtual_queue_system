d32c51ef3ce746f10e7176e9a8592100a9409d2b
Majd Alkawaas <mma250@mail.aub.edu>
2025-03-16 00:51:02 +0000

    Added the url patterns for guest registration and the guest waiting page

--- virtual_queue_system/guests/urls.py
+++ virtual_queue_system/guests/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [ 
+    path('enter_queue/<int:queue_id>', views.guest_page, name='enter_queue'),
+    path('guest_queue/<int:queue_id>/<int:guest_id>', views.queue_guest, name='queue_guest'),
 
 ]
